3c88d291277d8da0929a04ab7e8599b16b653770
Stephano Romain <stefano.romain@gmail.com>
2025-06-19 11:53:31 +0200

    feat: first cleanup

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -13,31 +13,27 @@ class GildedRose(object):
             if item.name == self.SULFURAS_HAND_OF_RAGNAROS:
                 continue
             
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name != self.AGED_BRIE and item.name != self.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT:
                 if item.quality > 0:
                     item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == self.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT:
                         if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                            item.quality = item.quality + 1
                         if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                            item.quality = item.quality + 1
             
             item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
+                if item.name == self.AGED_BRIE:
                     if item.quality < 50:
                         item.quality = item.quality + 1
+                elif item.name == self.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT:
+                    item.quality = item.quality - item.quality
+                elif item.quality > 0:
+                    item.quality = item.quality - 1
 
 
 class Item:
