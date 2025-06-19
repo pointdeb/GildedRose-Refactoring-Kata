de694b68f53326e1d691db60e31f029f7622b8c8
Stephano Romain <stefano.romain@gmail.com>
2025-06-19 11:01:38 +0200

    feat: make tests works

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,12 +5,70 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+
+    def get_fixture_items(self, name):
+        return [
+            Item(name, 0, 0),
+            Item(name, -1, -1),
+            Item(name, 5, 45),
+            Item(name, 6, 45),
+            Item(name, 7, 52),
+            Item(name, 12, 52),
+        ]
+
+    def check_asserts(self, items, expects):
+        self.assertEqual(len(items), len(expects))
+
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        for i, item in enumerate(items):
+            self.assertEqual(item.quality, expects[i].quality)
+            self.assertEqual(item.sell_in, expects[i].sell_in)
 
-        
-if __name__ == '__main__':
+    def test_01_custom_item(self):
+        name = "Custom"
+        items = self.get_fixture_items(name)
+        expects = [
+            Item(name, -1, 0),
+            Item(name, -2, -1),
+            Item(name, 4, 44),
+            Item(name, 5, 44),
+            Item(name, 6, 51),
+            Item(name, 11, 51),
+        ]
+        self.check_asserts(items, expects)
+
+    def test_02_aged_brie(self):
+        name = "Aged Brie"
+        items = self.get_fixture_items(name)
+        expects = [
+            Item(name, -1, 2),
+            Item(name, -2, 1),
+            Item(name, 4, 46),
+            Item(name, 5, 46),
+            Item(name, 6, 52),
+            Item(name, 11, 52),
+        ]
+        self.check_asserts(items, expects)
+
+    def test_03_fulfuras_hand_of_ragnaros(self):
+        name = "Sulfuras, Hand of Ragnaros"
+        items = self.get_fixture_items(name)
+        self.check_asserts(items, items)
+
+    def test_04_backstage_passes_to_a_tafkal(self):
+        name = "Backstage passes to a TAFKAL80ETC concert"
+        items = self.get_fixture_items(name)
+        expects = [
+            Item(name, -1, 0),
+            Item(name, -2, 0),
+            Item(name, 4, 48),
+            Item(name, 5, 47),
+            Item(name, 6, 52),
+            Item(name, 11, 52),
+        ]
+        self.check_asserts(items, expects)
+
+
+if __name__ == "__main__":
     unittest.main()
